ba10ed138d85ef19ec6d31252c440ee4e3c9e521
Joshua Byrd <phocks@gmail.com>
2023-06-26 22:57:12 +1000

    Web dictaphone demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [svelte()]
 });
