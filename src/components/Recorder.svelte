36211084ecbf0483b1150cfb3096a71f20bfbbe3
Joshua Byrd <phocks@gmail.com>
2023-06-28 00:06:38 +1000

    OK let's try this

--- src/components/Recorder.svelte
+++ src/components/Recorder.svelte
@@ -1,12 +1,150 @@
 <script lang="ts">
   import { onMount } from "svelte";
 
+  let record: HTMLButtonElement;
+  let stop: HTMLButtonElement;
+
+  let handleRequestAudioClick = () => {};
+
   onMount(() => {
-    console.log(":)");
+    const requestAudio = () => {
+      // Main block for doing the audio recording
+
+      if (navigator.mediaDevices.getUserMedia) {
+        console.log("getUserMedia supported.");
+
+        const constraints = { audio: true };
+        let chunks = [];
+
+        let onSuccess = function (stream) {
+          const mediaRecorder = new MediaRecorder(stream);
+
+          visualize(stream);
+
+          record.onclick = function () {
+            mediaRecorder.start();
+            console.log(mediaRecorder.state);
+            console.log("recorder started");
+            record.style.background = "red";
+
+            stop.disabled = false;
+            record.disabled = true;
+          };
+
+          stop.onclick = function () {
+            mediaRecorder.stop();
+            console.log(mediaRecorder.state);
+            console.log("recorder stopped");
+            record.style.background = "";
+            record.style.color = "";
+            // mediaRecorder.requestData();
+
+            stop.disabled = true;
+            record.disabled = false;
+          };
+
+          mediaRecorder.onstop = function (e) {
+            console.log("data available after MediaRecorder.stop() called.");
+
+            // const clipName = prompt(
+            //   "Enter a name for your sound clip?",
+            //   "My unnamed clip"
+            // );
+
+            const clipName = "new-clip"
+
+            const clipContainer = document.createElement("article");
+            const clipLabel = document.createElement("p");
+            const audio = document.createElement("audio");
+            const deleteButton = document.createElement("button");
+
+            clipContainer.classList.add("clip");
+            audio.setAttribute("controls", "");
+            deleteButton.textContent = "Delete";
+            deleteButton.className = "delete";
+
+            if (clipName === null) {
+              clipLabel.textContent = "My unnamed clip";
+            } else {
+              clipLabel.textContent = clipName;
+            }
+
+            clipContainer.appendChild(audio);
+            // clipContainer.appendChild(clipLabel);
+            // clipContainer.appendChild(deleteButton);
+            soundClips.appendChild(clipContainer);
+
+            audio.controls = true;
+            const blob = new Blob(chunks, { type: "audio/webm; codecs=opus" });
+            chunks = [];
+            const audioURL = window.URL.createObjectURL(blob);
+            audio.src = audioURL;
+            console.log("recorder stopped");
+
+            // Send blob to server
+            const formData = new FormData();
+
+            formData.append("file", blob, "audio.webm");
+
+            fetch("https://confessions.phocks.org/upload", {
+              //  fetch("http://localhost:5000/upload", {
+              method: "POST",
+              body: formData,
+              headers: {
+                // NOTE: Browsers will automatically set the Content-Type header for you
+                // So this is not needed
+                // "Content-Type": "multipart/form-data",
+              },
+            })
+              .then((res) => {
+                console.log(res);
+                // button.innerHTML = "Audio sent...";
+              })
+              .catch((err) => {
+                // button.innerHTML = "Error :(";
+                console.error("Error occured", err);
+              });
+
+            deleteButton.onclick = function (e: any) {
+              e.target.closest(".clip").remove();
+            };
+
+            clipLabel.onclick = function () {
+              const existingName = clipLabel.textContent;
+              const newClipName = prompt(
+                "Enter a new name for your sound clip?"
+              );
+              if (newClipName === null) {
+                clipLabel.textContent = existingName;
+              } else {
+                clipLabel.textContent = newClipName;
+              }
+            };
+          };
+
+          mediaRecorder.ondataavailable = function (e) {
+            chunks.push(e.data);
+          };
+        };
+
+        let onError = function (err) {
+          console.log("The following error occured: " + err);
+        };
+
+        navigator.mediaDevices
+          .getUserMedia(constraints)
+          .then(onSuccess, onError);
+      } else {
+        console.log("getUserMedia not supported on your browser!");
+      }
+    };
+
+    handleRequestAudioClick = requestAudio;
+
     // set up basic variables for app
 
-    const record: any = document.querySelector(".record");
-    const stop: any = document.querySelector(".stop");
+    // const record = recordButtonEl;
+    // const stop: any = document.querySelector(".stop");
     const soundClips: any = document.querySelector(".sound-clips");
     const canvas: any = document.querySelector(".visualizer");
     const mainSection: any = document.querySelector(".main-controls");
@@ -20,130 +158,8 @@
     let audioCtx;
     const canvasCtx = canvas.getContext("2d");
 
-    //main block for doing the audio recording
-
-    if (navigator.mediaDevices.getUserMedia) {
-      console.log("getUserMedia supported.");
-
-      const constraints = { audio: true };
-      let chunks = [];
-
-      let onSuccess = function (stream) {
-        const mediaRecorder = new MediaRecorder(stream);
-
-        visualize(stream);
-
-        record.onclick = function () {
-          mediaRecorder.start();
-          console.log(mediaRecorder.state);
-          console.log("recorder started");
-          record.style.background = "red";
-
-          stop.disabled = false;
-          record.disabled = true;
-        };
-
-        stop.onclick = function () {
-          mediaRecorder.stop();
-          console.log(mediaRecorder.state);
-          console.log("recorder stopped");
-          record.style.background = "";
-          record.style.color = "";
-          // mediaRecorder.requestData();
-
-          stop.disabled = true;
-          record.disabled = false;
-        };
-
-        mediaRecorder.onstop = function (e) {
-          console.log("data available after MediaRecorder.stop() called.");
-
-          const clipName = prompt(
-            "Enter a name for your sound clip?",
-            "My unnamed clip"
-          );
-
-          const clipContainer = document.createElement("article");
-          const clipLabel = document.createElement("p");
-          const audio = document.createElement("audio");
-          const deleteButton = document.createElement("button");
-
-          clipContainer.classList.add("clip");
-          audio.setAttribute("controls", "");
-          deleteButton.textContent = "Delete";
-          deleteButton.className = "delete";
-
-          if (clipName === null) {
-            clipLabel.textContent = "My unnamed clip";
-          } else {
-            clipLabel.textContent = clipName;
-          }
-
-          clipContainer.appendChild(audio);
-          clipContainer.appendChild(clipLabel);
-          clipContainer.appendChild(deleteButton);
-          soundClips.appendChild(clipContainer);
-
-          audio.controls = true;
-          const blob = new Blob(chunks, { type: "audio/webm; codecs=opus" });
-          chunks = [];
-          const audioURL = window.URL.createObjectURL(blob);
-          audio.src = audioURL;
-          console.log("recorder stopped");
-
-          // Send blob to server
-          const formData = new FormData();
-
-          formData.append("file", blob, "audio.webm");
-
-          
-          fetch("https://confessions.phocks.org/upload", {
-            //  fetch("http://localhost:5000/upload", {
-            method: "POST",
-            body: formData,
-            headers: {
-              // NOTE: Browsers will automatically set the Content-Type header for you
-              // So this is not needed
-              // "Content-Type": "multipart/form-data",
-            },
-          })
-            .then((res) => {
-              console.log(res);
-              // button.innerHTML = "Audio sent...";
-            })
-            .catch((err) => {
-              // button.innerHTML = "Error :(";
-              console.error("Error occured", err);
-            });
-
-          deleteButton.onclick = function (e: any) {
-            e.target.closest(".clip").remove();
-          };
-
-          clipLabel.onclick = function () {
-            const existingName = clipLabel.textContent;
-            const newClipName = prompt("Enter a new name for your sound clip?");
-            if (newClipName === null) {
-              clipLabel.textContent = existingName;
-            } else {
-              clipLabel.textContent = newClipName;
-            }
-          };
-        };
-
-        mediaRecorder.ondataavailable = function (e) {
-          chunks.push(e.data);
-        };
-      };
-
-      let onError = function (err) {
-        console.log("The following error occured: " + err);
-      };
-
-      navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
-    } else {
-      console.log("getUserMedia not supported on your browser!");
-    }
+    // Request audio
+    // requestAudio();
 
     function visualize(stream) {
       if (!audioCtx) {
@@ -158,7 +174,6 @@
       const dataArray = new Uint8Array(bufferLength);
 
       source.connect(analyser);
-      //analyser.connect(audioCtx.destination);
 
       draw();
 
@@ -208,17 +223,65 @@
 </script>
 
 <div class="wrapper">
-  <!-- <header>
-    <h1>Web dictaphone</h1>
-  </header> -->
+  <div class="main">
+    <section class="main-controls">
+      <canvas class="visualizer" height="60px" />
+      <div id="buttons">
+        <button on:click={handleRequestAudioClick}>
+          Request recording permissions
+        </button>
+        <button bind:this={record} class="record">Record</button>
+        <button bind:this={stop} class="stop">Stop</button>
+      </div>
+    </section>
 
-  <section class="main-controls">
-    <canvas class="visualizer" height="60px" />
-    <div id="buttons">
-      <button class="record">Record</button>
-      <button class="stop">Stop</button>
-    </div>
-  </section>
-
-  <section class="sound-clips" />
+    <section class="sound-clips" />
+  </div>
 </div>
+
+<style lang="scss">
+  .wrapper {
+    // display: flex;
+    // flex-direction: column;
+    // align-items: center;
+    // justify-content: center;
+    // height: 100dvh;
+  }
+
+  canvas {
+    width: 100%;
+    height: 60px;
+    background-color: #333;
+  }
+
+  .main {
+    width: 100%;
+    margin: 0 auto;
+  }
+
+  .main-controls {
+    width: 100%;
+    margin-block-end: 1em;
+  }
+
+  .request-audio {
+    padding: 1rem;
+  }
+
+  .visualizer {
+    vertical-align: middle;
+  }
+
+  #buttons {
+    margin-block-start: 1em;
+  }
+
+  button {
+    display: inline-block;
+    margin-block-end: 10px;
+    min-width: 100px;
+    background-color: white;
+    border: 1px solid black;
+    padding: 0.5rem 1rem;
+  }
+</style>
